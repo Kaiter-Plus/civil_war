101f9d066c8497dcdae41ebc7740ed12f82d8bf7
Kaiter-Plus <bundows@foxmail.com>
2026-04-09 07:28:31 +0800

    build: migrate from webpack to vite
    - Replace webpack with vite for faster builds
    - Remove 23 deprecated dependencies (eslint-loader, url-loader, etc.)
    - Simplify build config from 100+ lines to 20 lines
    - Update imports to ESM syntax
    - Fix CSS image paths for vite compatibility
    
    Build output: 144KB JS (gzip 60KB), 244KB CSS (gzip 38KB)

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,40 @@ import '../css/index.css';
 import 'bootstrap/js/dist/modal';
 import $ from 'jquery';
 
+// 导入资源
+import logoImg from '../assets/img/logo.png';
+import ambulanceImg from '../img/ambulance.png';
+import capsuleImg from '../img/capsule.png';
+import virusImg from '../img/virus.png';
+import killedImg from '../assets/img/killed.png';
+import isDestroyImg from '../assets/img/isDestroy.png';
+import bgImg from '../img/bg.jpeg';
+
+// 导入 JSON 数据
+import rulesData from '../assets/json/rules.json';
+import knowledgeData from '../assets/json/knowledge.json';
+import rumorsData from '../assets/json/rumors.json';
+
+// 导入音频
+import buttonMp3 from '../music/buttonDown/button.mp3';
+import startMp3 from '../music/start/start.mp3';
+import bgMusicMp3 from '../music/backgroundMusic/backgroundMusic.mp3';
+import shotMp3 from '../music/shot/shot.mp3';
+import killedMp3 from '../music/killed/killed.mp3';
+import gameOverMp3 from '../music/gameOver/gameOver.mp3';
+
 // 游戏对象
 const game = {
   // 初始容器对象
   stage: $('#stage'),
 
   // 音效对象
-  buttonDown: $('#buttonDown'),
-  start: $('#start'),
-  backgroundMusic: $('#backgroundMusic'),
-  shot: $('#shot'),
-  killed: $('#killed'),
-  gameOver: $('#gameOver'),
+  buttonDown: null,
+  start: null,
+  backgroundMusic: null,
+  shot: null,
+  killed: null,
+  gameOver: null,
 
   // 模态框标题
   modalTitle: $('.modal-title'),
@@ -26,9 +48,9 @@ const game = {
     startGame: null,
   },
 
-  // 游戏模式参数配置，修改此处可以修改难度
+  // 游戏模式参数配置
   mode: [
-    // [胶囊速度, 胶囊发射间隔, 病毒速度最小, 病毒速度最大值, 病毒出现时间间隔] 单位病毒出现间隔：ms 病毒速度越小越快，难度越难
+    // [胶囊速度, 胶囊发射间隔, 病毒速度最小, 病毒速度最大值, 病毒出现时间间隔]
     [2000, 260, 1500, 4500, 600],
   ],
 
@@ -38,6 +60,11 @@ const game = {
     ambulanceY: 0,
     score: 0,
   },
+
+  // 数据
+  rules: rulesData,
+  knowledge: knowledgeData,
+  rumors: rumorsData,
 
   // 控制救护车移动
   control(e) {
@@ -84,64 +111,52 @@ const game = {
   // 开始界面按钮点击事件
   buttonClick(index, showMessage) {
     switch (index) {
-      // 开始游戏
       case 0: {
-        // 游戏开始标志
         game.stage.start = true;
-        // 设置鼠标不可见
         game.stage.css('cursor', 'none');
-        // 移除开始界面
         game.startScreen.remove();
+        
         const ready = $("<div id='ready'>")
           .text('Ready')
           .stop()
           .animate(
-            {
-              fontSize: '5em',
-            },
+            { fontSize: '5em' },
             1000,
             () => {
               ready.text('GO!').stop().fadeTo(1000, 'hide');
             },
           );
         game.stage.append(ready);
-        // 按钮点击音效
+        
         game.buttonDown.get(0).play();
-        // 开始游戏音效
-        $('#start').get(0).play();
-        // 降低胶囊发射音量
-        $('#shot').get(0).volume = 0.4;
-        // 播放背景音乐
+        game.start.get(0).play();
+        game.shot.get(0).volume = 0.4;
+
         game.timer.backgroundMusic = setTimeout(() => {
-          // 开始播放背景音乐
           game.backgroundMusic.get(0).load();
           game.backgroundMusic.get(0).play();
           clearTimeout(game.timer.backgroundMusic);
         }, 2000);
-        // 控制救护车
+
         if (/Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent)) {
-          // 移动端
           $(document).on('touchmove', (e) => {
             game.control(e.originalEvent.changedTouches[0]);
           });
         } else {
-          // PC端
           $(document).mousemove((e) => {
             game.control(e);
           });
         }
+
         const set = game.mode[index];
 
         game.timer.startGame = setTimeout(() => {
-          // 调用函数绘制救护车以及分数栏
           game.core.draw();
 
-          // 胶囊发射定时器
           game.timer.capsule = setInterval(() => {
             game.core.capsule(set[0], [game.num.ambulanceX, game.num.ambulanceY]);
           }, set[1]);
 
-          // 病毒出现定时器
           game.timer.virus = setInterval(() => {
             game.core.virus({
               speed: game.randomNum(set[2], set[3]),
@@ -149,17 +164,16 @@ const game = {
               top: -game.randomNum(32, 64),
             });
           }, set[4]);
+          
           clearTimeout(game.timer.startGame);
         }, 2000);
         break;
       }
-      // 展示游戏规则
       case 1: {
         game.modalTitle.html('游戏规则');
         showMessage.html(game.rules.join('\n'));
         break;
       }
-      // 展示防疫小知识
       case 2: {
         game.modalTitle.html('防疫小知识');
         showMessage.html(
@@ -169,7 +183,6 @@ const game = {
         );
         break;
       }
-      // 展示谣言以及真相
       case 3: {
         game.modalTitle.html('谣言我先知');
         showMessage.html(
@@ -189,23 +202,20 @@ const game = {
 
   // 开始场景
   startScreen: {
-    // 绘制开始界面
     draw() {
-      // 绘制标题
       const title = $('<div>')
         .addClass('title')
         .append(
           $("<div class='logo'>").append(
             $("<img alt='全民战疫' src=''>").prop({
               class: 'img-fluid',
-              src: './assets/img/logo.png',
+              src: logoImg,
               style: 'user-select: none',
             }),
           ),
         );
       game.stage.append(title);
 
-      // 创建选项
       const buttonGroup = $('<div>').addClass('buttonGroup col-8').html(`
             <a class='btn btn-block btn-lg btn-success' href='javascript:;'>开始游戏</a>
             <a class='btn btn-block btn-lg btn-primary' href='javascript:;' data-bs-toggle='modal' data-bs-target='#modal'>查看规则</a>
@@ -214,7 +224,6 @@ const game = {
           `);
       game.stage.append(buttonGroup);
 
-      // 事件委派
       buttonGroup.on('click', 'a', (e) => {
         const that = e.target;
         const showMessage = $('#showMessage');
@@ -222,15 +231,9 @@ const game = {
       });
     },
 
-    // 界面元素移除
     remove() {
       const removeDiv = game.stage.children($('div'));
-      removeDiv.stop().animate(
-        {
-          opacity: 0,
-        },
-        100,
-      );
+      removeDiv.stop().animate({ opacity: 0 }, 100);
       setTimeout(() => {
         removeDiv.remove();
       }, 300);
@@ -239,80 +242,54 @@ const game = {
 
   // 核心代码
   core: {
-    // 绘制救护车以及分数栏
     draw() {
-      // 绘制救护车
       const ambulance = $('<div>');
       ambulance.addClass('ambulance').css({
+        'background-image': `url(${ambulanceImg})`,
         top: game.stage.height() / 2 + 64,
         left: game.stage.width() / 2 - 15,
       });
       game.stage.append(ambulance);
-      // 绘制分数栏
+
       const score = $('<div>').addClass('score').html('0');
       game.stage.append(score);
 
-      // 新建一个隐藏按钮,用于触发结算界面的防疫小知识
-      const tipButton = $('<button id=\'tipButton\' style=\'display:none\' data-bs-toggle=\'modal\' data-bs-target=\'#modal\'>');
+      const tipButton = $("<button id='tipButton' style='display:none' data-bs-toggle='modal' data-bs-target='#modal'>");
       game.stage.append(tipButton);
 
-      // 记录胶囊发射初始位置
       game.num.ambulanceX = parseInt(ambulance.css('left'), 10) + ambulance.width() / 2;
       game.num.ambulanceY = parseInt(ambulance.css('top'), 10);
     },
 
-    // 救护车位置
     ambulance(pos) {
       const ambulance = game.stage.find($('.ambulance'));
       let left = pos[0] - ambulance.width() / 2;
       let top = pos[1] - ambulance.height() / 2;
 
-      // 防止救护车跑出画面之外
-      if (left <= 0) {
-        left = 0;
-      } else if (left >= game.stage.width() - ambulance.width()) {
-        left = game.stage.width() - ambulance.width();
-      }
-      if (top <= 0) {
-        top = 0;
-      } else if (top >= game.stage.height() - ambulance.height()) {
-        top = game.stage.height() - ambulance.height();
-      }
+      if (left <= 0) left = 0;
+      else if (left >= game.stage.width() - ambulance.width()) left = game.stage.width() - ambulance.width();
+      if (top <= 0) top = 0;
+      else if (top >= game.stage.height() - ambulance.height()) top = game.stage.height() - ambulance.height();
 
-      ambulance.css({
-        left,
-        top,
-      });
-
-      // 记录胶囊发射初始位置
+      ambulance.css({ left, top });
       game.num.ambulanceX = left + ambulance.width() / 2;
       game.num.ambulanceY = top;
     },
 
-    // 绘制胶囊并开始发射动画
     capsule(speed, pos) {
-      // 胶囊发射音效
-      $('#shot').get(0).play();
+      game.shot.get(0).play();
       const capsule = $('<div>');
-      capsule.addClass('capsule');
+      capsule.addClass('capsule').css('background-image', `url(${capsuleImg})`);
       game.stage.append(capsule);
       capsule.css({
         left: pos[0] - capsule.width() / 2,
         top: pos[1] - capsule.height() / 2,
       });
-      capsule.stop().animate(
-        {
-          top: -capsule.height(),
-        },
-        speed,
-        'linear',
-        () => {
-          capsule.remove();
-        },
-      );
+      capsule.stop().animate({ top: -capsule.height() }, speed, 'linear', () => {
+        capsule.remove();
+      });
     },
 
-    // 病毒
     virus(argument) {
       const { speed } = argument;
       const { left } = argument;
@@ -321,10 +298,12 @@ const game = {
       $virus
         .addClass('virus')
         .css({
+          'background-image': `url(${virusImg})`,
           left,
           top,
         })
         .appendTo(game.stage);
+      
       $virus.stop().animate(
         {
           top: '100%',
@@ -336,27 +315,20 @@ const game = {
           clearInterval($virus.timer);
         },
       );
+
       $virus.timer = setInterval(() => {
-        // 获取救护车对象
         const ambulance = $('.ambulance');
-        // 获取屏幕上的所有胶囊
         const capsule = $('.capsule');
-        // 胶囊击中，病毒消失
         const x = parseInt($virus.css('left'), 10) + 32;
         const y = parseInt($virus.css('top'), 10) + 32;
         const l = capsule.length;
 
-        // 病毒击中消失
         for (let i = 0; i < l; i += 1) {
           const dx = Math.abs(x - parseInt(capsule.eq(i).css('left'), 10) - 8);
           const dy = Math.abs(y - parseInt(capsule.eq(i).css('top'), 10) - 13);
           if (dx <= 40 && dy <= 45) {
-            // 病毒击中效果
-            $virus.css('background', "url('./assets/img/killed.png')");
-            // 击毁病毒音效
+            $virus.css('background-image', `url(${killedImg})`);
             game.killed.get(0).play();
-
-            // 移除胶囊
             capsule.eq(i).remove();
             clearInterval($virus.timer);
             game.num.score += 1;
@@ -367,22 +339,18 @@ const game = {
           }
         }
 
-        // 救护车损坏，显示分数以及成就
         const dx2 = Math.abs(x - parseInt(ambulance.css('left'), 10) - 15);
         const dy2 = Math.abs(y - parseInt(ambulance.css('top'), 10) - 32);
 
-        // !($(".tips").get(0)) 用于防止创建多个 tips
         if (dx2 <= 47 && dy2 <= 64 && !$('.tips').get(0)) {
-          // 设置鼠标可见
           game.stage.css('cursor', 'default');
-          // 播放游戏结束音乐
-          $('#gameOver').get(0).play();
-          // 暂停音乐
-          $('#backgroundMusic').get(0).pause();
-          // 结算
+          game.gameOver.get(0).play();
+          game.backgroundMusic.get(0).pause();
+
           const tips = $('<div>');
           tips.addClass('tips alert').css('display', 'none');
           const score = $('.score');
+          
           switch (String(game.num.score).length) {
             case 1:
               tips.addClass('alert-danger');
@@ -402,7 +370,6 @@ const game = {
           }
           game.stage.append(tips);
 
-          // 结算时的防疫小知识
           game.stage.on('click', '#tipButton', () => {
             game.modalTitle.html('悄悄告诉你：');
             $('#showMessage').html(
@@ -413,27 +380,23 @@ const game = {
           });
           $('#tipButton').get(0).click();
 
-          // 监听 再来一次 点击事件，事件委派给 stage
           game.stage.on('click', '.tips p', () => {
             game.num.score = 0;
             game.startScreen.remove();
             game.startScreen.draw();
           });
 
-          // 移除
           $virus.remove();
           score.addClass('none');
-          ambulance.css('background', "url('./assets/img/isDestroy.png')");
+          ambulance.css('background-image', `url(${isDestroyImg})`);
           clearInterval($virus.timer);
           setTimeout(() => {
             $('.ambulance').remove();
           }, 400);
 
-          // 清除时间轮
           clearInterval(game.timer.capsule);
           clearInterval(game.timer.virus);
 
-          // 2s 后显示结算信息
           setTimeout(() => {
             tips.css('display', 'block');
           }, 1000);
@@ -442,7 +405,6 @@ const game = {
     },
   },
 
-  // 产生指定区域整形随机数。
   randomNum(a, b) {
     const value = Math.abs(a - b);
     const num = Math.floor(Math.random() * value) + Math.min(a, b);
@@ -450,45 +412,24 @@ const game = {
   },
 };
 
+// 初始化音频元素
+function initAudio() {
+  game.buttonDown = $('#buttonDown');
+  game.start = $('#start');
+  game.backgroundMusic = $('#backgroundMusic');
+  game.shot = $('#shot');
+  game.killed = $('#killed');
+  game.gameOver = $('#gameOver');
+
+  game.buttonDown.append(`<source src="${buttonMp3}" type="audio/mpeg">`);
+  game.start.append(`<source src="${startMp3}" type="audio/mpeg">`);
+  game.backgroundMusic.append(`<source src="${bgMusicMp3}" type="audio/mpeg">`);
+  game.shot.append(`<source src="${shotMp3}" type="audio/mpeg">`);
+  game.killed.append(`<source src="${killedMp3}" type="audio/mpeg">`);
+  game.gameOver.append(`<source src="${gameOverMp3}" type="audio/mpeg">`);
+}
+
 $(() => {
-  // 初始化数据
-  (async function init() {
-    // 游戏规则
-    await $.ajax({
-      type: 'GET',
-      url: './assets/json/rules.json',
-      dataType: 'json',
-      success(data) {
-        game.rules = data;
-      },
-      error() {
-        game.rules = ['不好意思，加载本地文件失败了'];
-      },
-    });
-    // 防疫小知识
-    await $.ajax({
-      type: 'GET',
-      url: './assets/json/knowledge.json',
-      dataType: 'json',
-      success(data) {
-        game.knowledge = data;
-      },
-      error() {
-        game.knowledge = ['不好意思，加载本地文件失败了'];
-      },
-    });
-    // 谣言和真相
-    await $.ajax({
-      type: 'GET',
-      url: './assets/json/rumors.json',
-      dataType: 'json',
-      success(data) {
-        game.rumors = data;
-      },
-      error() {
-        game.rumors = [['不好意思，加载本地文件失败了', '不好意思，加载本地文件失败了']];
-      },
-    });
-    game.startScreen.draw();
-  }());
+  initAudio();
+  game.startScreen.draw();
 });
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: 'src',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'js/[name].[hash].js',
+        entryFileNames: 'js/[name].[hash].js'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
